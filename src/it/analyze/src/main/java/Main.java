b2b1beee3f10db7ec1b8574db038eca7ef5f6dfa
sbashkyrtsev <sbashkyrtsev@griddynamics.com>
2013-07-01 18:09:53 +0400

    initial commit from maven-dependency-plugin svn repo

---
+++ src/it/analyze/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.repository.metadata.Metadata;
+import org.apache.maven.model.Model;
+
+public class Main
+{
+    public static final String SCOPE_COMPILE = Artifact.SCOPE_COMPILE;
+
+    public Model model = null;
+
+    public Metadata metadata = null;
+}
